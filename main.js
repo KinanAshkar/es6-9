9a3a50e4b6e141fe54bfadbba80f7367d08591c9
KinanAshkar <kinan-ashkar@live.com>
2021-02-05 02:11:12 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Then & Catch & Training:
+
+/* const myPromise = new Promise((resolve, reject) => {
+    // only one state allowed
+    // resolve("resolved");
+    // reject("rejected"); // ignore
+    if(Math.random() * 100 < 90){
+        resolve("good");
+    }else{
+        reject("bad");
+    }
+});
+
+myPromise.then(
+        (myResolved) => console.log(myResolved),
+        (myRejected) => console.log(myRejected)
+);
+
+// myPromise.then(
+//         null,
+//         (myRejected) => console.log(myRejected)
+// );
+
+
+// myPromise.catch(
+//     (myRejected) => console.log(myRejected)
+// ); */
+
+// _____________________________
